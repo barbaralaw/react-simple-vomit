a0029381a583a2315bce6fcc2de96ba332e0a5cf
Barbara <64677474+barbaralaw@users.noreply.github.com>
2021-06-03 17:10:07 -0700

    commenting for understanding

--- src/SectionMain.js
+++ src/SectionMain.js
@@ -1,9 +1,15 @@
+// imports react components from react
 import React, { Component } from 'react';
+// imports sectionMain styling
 import './SectionMain.css';
 
+// creates new component sectionMain
 class SectionMain extends Component {
+  // will render the following
   render() {
+    // will return the following html
     return (
+        // a section with class name sectionMain
         <section className="SectionMain">
         </section>
 
@@ -11,4 +17,5 @@ class SectionMain extends Component {
   }
 }
 
+// exports single component sectionMain
 export default SectionMain;
